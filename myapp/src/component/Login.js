96418ee8408fb53260ac46e2d7c61100fa461720
iamsohailsp <sohelpathan0033@gmail>
2023-12-16 21:03:04 +0530

    hello

---
+++ myapp/src/component/Login.js
@@ -0,0 +1,48 @@
+// Login.js
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  
+
+  const handleLogin = () => {
+    console.log(username)
+  console.log(password)
+    // Add your login logic here
+    console.log('Logging in...');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
+
