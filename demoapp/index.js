202470fb288727514c683761765740e9fb4d6dac
iamsohailsp <sohelpathan0033@gmail>
2023-12-19 16:54:59 +0530

    sp

--- demoapp/index.js
+++ demoapp/index.js
@@ -10,9 +10,6 @@ app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 app.use(router); // Use your defined routes
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 const PORT = process.env.PORT || 3000;
 
